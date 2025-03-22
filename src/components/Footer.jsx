ece6c98ec910fa0bef836e0b8f72646220c4109a
Safira I A <auliasafiraisma@gmail.com>
2025-03-22 16:13:23 +0700

    feat: footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import TechComponentSmall from "./template/TechComponentSmall";
+
+export default function Footer() {
+    const technologies = ["React", "Tailwind CSS"];
+
+    return (
+        <>
+            <div className="border-1 border-stone-200 w-full mx-auto"></div>
+            <footer className="flex justify-between items-center w-full py-6 bg-black text-white text-center font-fira-sans">
+                <p className="text-sm opacity-75">
+                    &copy; {new Date().getFullYear()} Built by SafiraIA.
+                </p>
+
+                <div className="mt-2 flex justify-center items-center gap-2">
+                    <span className="text-sm opacity-75">Portfolio Made With:</span>
+                    {technologies.map((skill, index) => (
+                        <TechComponentSmall key={index} name={skill} />
+                    ))}
+                </div>
+            </footer>
+        </>
+    );
+}
